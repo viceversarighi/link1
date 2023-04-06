1975800f50f935e04cae52051ae7158496f3cafb
viceversarighi <130054964+viceversarighi@users.noreply.github.com>
2023-04-06 13:26:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.querySelector(".btn-1").addEventListener("click", function () {
+  window.open(
+    "https://docs.google.com/forms/d/15SMaUs7wQ1TdQNbRx01IbGw5ykwPfkvdMak0tA_kbiE/edit",
+    "_blank"
+  );
+});
+
+document.querySelector(".btn-2").addEventListener("click", function () {
+  window.open("https://miro.com/app/board/uXjVPhhFcEE=/", "_blank");
+});
