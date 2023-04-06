c84f613533885e7a2aa4af6cf91a17f39cd16db1
viceversarighi <130054964+viceversarighi@users.noreply.github.com>
2023-04-06 13:20:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 "use strict";
 
 document.querySelector(".btn-1").addEventListener("click", function () {
-  window.open("www.google.com", "_blank");
+  window.open(
+    "https://docs.google.com/forms/d/15SMaUs7wQ1TdQNbRx01IbGw5ykwPfkvdMak0tA_kbiE/edit",
+    "_blank"
+  );
 });
 
 document.querySelector(".btn-2").addEventListener("click", function () {
-  window.open("www.turnyournameintoaface.com", "_blank");
+  window.open("https://miro.com/app/board/uXjVPhhFcEE=/", "_blank");
 });
