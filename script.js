428170e1d0f2ab54bba811752cc5e8902ed779a8
viceversarighi <130054964+viceversarighi@users.noreply.github.com>
2023-04-06 12:57:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.querySelector(".btn-1").addEventListener("click", function () {
+  window.open("www.google.com", "_blank");
+});
+
+document.querySelector(".btn-2").addEventListener("click", function () {
+  window.open("www.turnyournameintoaface.com", "_blank");
+});
